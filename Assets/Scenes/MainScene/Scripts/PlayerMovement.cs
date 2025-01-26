7fc03e5dd73cbf2e1dee918122b50ca770c6980d
Nikita <nikita.semonin@gmail.com>
2025-01-26 14:04:38 +0200

    done main anim

--- Assets/Scenes/MainScene/Scripts/PlayerMovement.cs
+++ Assets/Scenes/MainScene/Scripts/PlayerMovement.cs
@@ -30,7 +30,8 @@ namespace Scenes.MainScene.Scripts
             float horizontal = Input.GetAxisRaw("Horizontal"); // A (-1) and D (1)
             float vertical = Input.GetAxisRaw("Vertical");     // W (1) and S (-1)
             
-            // animator.SetFloat("Horizontal", horizontal);
+            // animator.SetInteger("horizontal", (int) horizontal);
+            animator.SetInteger("horizontal", horizontal > 0.1 ? 1 : horizontal < -0.1 ? -1 : 0);
             // animator.SetFloat("Vertical", vertical);
 
             // Create a movement vector
